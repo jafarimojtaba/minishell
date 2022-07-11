d40c133eb4b115844aee58536c0f48cb223b9fc9
Mojtaba Jafari <mjafari@c2s12d63.42wolfsburg.de>
2022-07-11 22:32:45 +0200

    v01: complete the parser.c-> make_command_string functions

---
+++ v01/main.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjafari <mjafari@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/09 15:44:35 by mjafari           #+#    #+#             */
+/*   Updated: 2022/07/11 21:40:27 by mjafari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishel.h"
+
+char *command_buff;
+
+int main(void)
+{
+// char *command_buff;
+
+	t_com	*commands;
+	int		n_pipes;
+	// argc = 0;
+	// int fdd = 0;
+	// argv = NULL;
+	char **splitted;
+	int i = 0;
+	
+	// buff = (char *)malloc(6 * sizeof(char));
+	// char *argVec[] = {"echo", "ehsan", NULL};
+	// char *enVec[] = {NULL};
+	// ft_env(env);
+	while (1)
+	{
+		command_buff = readline("minishell>");
+		if (ft_strlen(command_buff) > 0)
+			add_history(command_buff);
+		splitted = parser(command_buff);
+		// printf("command = %s\n", command_buff);
+		n_pipes = pipe_counter(splitted);
+		commands = (t_com *)malloc((n_pipes + 1) * sizeof(t_com));
+		search_commands(splitted, n_pipes, commands);
+		// printf("pipes are = %d\n", n_pipes);
+		// argVec[1] = command_buff;
+		// dup2(fdd, STDOUT_FILENO);
+		// execve("/bin/echo" , argVec, enVec);
+		// write(fdd, )
+		// printf("%s",buff);
+		free_splitted(splitted);
+	}
+
+	// command_buff = readline("minishell: ");
+	// printf("%s", getenv("PATH"));
+	return (0);
+}
---
+++ v01/parser.c
@@ -0,0 +1,143 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjafari <mjafari@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/10 12:23:38 by mjafari           #+#    #+#             */
+/*   Updated: 2022/07/11 22:31:34 by mjafari          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int check_end_before_pipe(char *cb, int i, char q)
+{
+	i++;
+	while (cb[i] && cb[i] != q && cb[i] != '|')
+	{
+		i++;
+		if (cb[i] == '\0' || cb[i] == '|')
+			return (0);
+		if (cb[i] == q)
+			return (i);
+	}
+	return (i);
+}
+
+int check_end_before_space_or_pipe(char *cb, int i)
+{
+	i++;
+	while (cb[i] && cb[i] != '|' && cb[i] != ' ')
+		i++;
+	return (i);
+}
+
+char **pre_parser(char *commandbuff)
+{
+	int i;
+	int end;
+	char **splitted;
+
+	i = 0;
+	while(commandbuff[i])
+	{
+		if (commandbuff[i] == ' ')
+			i++;
+		else if (commandbuff[i] == '\'')
+		{
+			end = check_end_before_pipe(commandbuff, i, '\'');
+			if (end)
+			{
+				make_command_str(commandbuff[i], commandbuff[end], splitted);
+				i = end;
+			}
+			else
+				return(error_of_not_closing_quotes);
+		}
+		else if (commandbuff[i] == '"')
+		{
+			end = check_end_before_pipe(commandbuff, i, '"');
+			if (end)
+			{
+				make_command_str_and_replace_dollar_value(commandbuff[i], commandbuff[end], splitted);
+				i = end;
+			}
+			else
+				return(error_of_not_closing_quotes);
+		}
+		else
+		{
+			end = check_end_before_space_or_pipe(commandbuff, i);
+			if (end)
+			{
+				make_command_str_and_replace_dollar_value(commandbuff[i], commandbuff[end], splitted);
+				i = end;
+			}
+		}			
+	}
+	free(commandbuff);
+	return(splitted);
+}
+
+// int	pipe_counter(char **splitted)
+// {
+// 	int	i;
+// 	int	c;
+
+// 	i = 0;
+// 	c = 0;
+// 	while (splitted[i])
+// 	{
+// 		printf("%s\n", splitted[i]);
+// 		if (splitted[i][0] == '|' && ft_strlen(splitted[i]) == 1)
+// 			c++;
+// 		i++;
+// 	}
+// 	return (c);
+// }
+
+// void	ft_make_command(char **splitted, t_com *commands)
+// {
+// 	int i;
+// 	i = 0;
+
+// 	while (splitted[i][0] != '|')
+// 	{
+// 		/* code */
+// 	}
+	
+// 	//if id = -1 then we can fill that command
+// }
+
+// int		ft_search_builtins(char **splitted, int *i, t_com *commands)
+// {
+// 	int ret;
+// 	// ret = ft_strcmp2d(splitted[i], {"echo", "cd", "pwd", "export", "unset", "env", "exit"});
+// 	if (ft_strncmp("echo", splitted[*i], ft_strlen(splitted[*i])))
+// 		if (ft_strncmp("-n", splitted[*i + 1], ft_strlen(splitted[*i + 1])))
+// 			ft_make_command(splitted, *i, commands);
+// }
+
+// void	search_commands(char **splitted, int n_pipes, t_com *commands)
+// {
+// 	int	i;
+// 	int	a_ret;
+// 	char **b_path;
+
+// 	i = 0;
+// 	n_pipes = 0;
+// 	b_path = ft_split(getenv("PATH"), ':');
+// 	// while (splitted[i])
+// 	// {
+// 	// 	ft_make_command()
+// 	// 	if (!ft_search_builtins(splitted, i, commands))
+// 	// 	{
+// 	// 		ft_search_shell(splitted[i]);
+// 	// 	}
+// 	// 	a_ret = access("/bin/echo", R_OK);
+// 	// 	printf("access key= %d\n", a_ret);
+// 	// 	i++;
+// 	// }
+// }
