fdb2d035eb6c84612cc00901dc549173cb9fc287
mojtaba <mojtabaj.official@gmail.com>
2022-08-06 00:19:49 +0200

    env_dup null terminated

--- env_utils.c
+++ env_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   copy_env.c                                         :+:      :+:    :+:   */
+/*   env_utils.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mjafari <mjafari@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 23:44:09 by mjafari           #+#    #+#             */
-/*   Updated: 2022/08/06 00:07:05 by mjafari          ###   ########.fr       */
+/*   Updated: 2022/08/06 00:19:00 by mjafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,14 @@ void copy_env(t_data *data, char **env, int i)
 
     while (env[i])
         i++;
-    env_dup = (char **)malloc((i + 1) * sizeof(char *));
+    env_dup = (char **)malloc((i + 2) * sizeof(char *));
     i = 0;
     while (env[i])
     {
         env_dup[i] = ft_strdup(env[i]);
-        // puts(env_dup[i]);
         i++;
     }
     env_dup[i] = ft_strjoin("OLDPWD=", getcwd(buf, 500));
-    // puts(env_dup[i]);
+    env_dup[i + 1] = NULL;
     data->env = env_dup;
 }
